2f40df59c86cbca1eb6ed020869ac878d3cc4c90
Andy <duyanhnn@outlook.com>
2025-02-13 23:03:55 +0700

    first commit

---
+++ src/main/java/com/onlyu/cachingv2/CachingV2Application.java
@@ -0,0 +1,13 @@
+package com.onlyu.cachingv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingV2Application.class, args);
+    }
+
+}
